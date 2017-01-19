2014a84e136e37054a1a0ae3bdae23bf9f0ab004
Brynna <brynna.mccarter@gmail.com>
2017-01-19 15:08:34 -0800

    fix small spacing and indentation errors in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
 //business logic
 
-//romans is an array containing all of the characters to convert to:
+//romans is an array containing all of the roman numeral characters to convert to:
 
-var romans = new Array();
-romans = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+var romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
 
 //decimals is an array containing the numbers that will be converted to their corresponding numeral
 
-var decimals = new Array();
-decimals = ["1000","900","500","400","100","90","50","40","10","9","5","4","1"];
+var decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
 
 //the variable converter is equal to a function that takes the parameter of numberInput(defined below in UI)
@@ -22,24 +20,26 @@ var converter = function(numberInput) {
     return numberInput;
   }
 
-//the variable romanNumeral is defined as an empty string
+//the variable result is defined as an empty string
 
-    var romanNumeral = "";
+    var result = "";
 
 //this for statement takes three parameters-the initialization parameter is a variable called i which is set to 0 (this tells the for loop where to start)-the condition parameter tells the loop to keep running as long as the variable i is less than the total length (note: .length is not a method, but rather a property of an array) of the romans array(12; so it will stop at the end of the romans array)-the final expression parameter is i++ which will change the initial value of i(initializing at 0) by one increment each time the loop is executed//
-    for (var i=0; i < romans.length; i++) {
+  for (var i = 0; i <= romans.length; i++) {
+
 
 //this while loop tells the for loop to run as long as the following conditions in the block of code are true. The while loop runs while numberInput is less than or equal to the current value of i in the decimal array.
-      while (numberInput >= decimals[i]) {
+    while (numberInput >= decimals[i]) {
 
-//if the while condition is true, the two statements below are executed. numberInput is decreased by whatever value is currently assinged to decimals at the current position in the array. It also makes romanNumeral equal to whichever value is currently held by i in the romans array.//
-        numberInput -= decimals[i];
-        romanNumeral += romans[i];
-      }
+
+//if the while condition is true, the two statements below are executed. numberInput is decreased by whatever value is currently assinged to decimals at the current position in the array. It also makes result equal to whichever value is currently held by i in the romans array.//
+      numberInput -= decimals[i];
+      result += romans[i];
     }
+  }
 
-//when the for/while loop is finished running, it returns the new value of romanNumeral
-    return romanNumeral;
+//when the for/while loop is finished running, it returns the new value of result
+    return result;
   };
 
 //user interface
