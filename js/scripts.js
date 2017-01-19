1637335bebe83408eb87516a6a902b6fee673c5b
Brynna <brynna.mccarter@gmail.com>
2017-01-19 10:09:15 -0800

    add user interface logic, business logic to handle first spec (numbers 1-3), spec 2 (0, above 4000)

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,42 @@
+//business logic
+var romans = new Array();
+romans = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+
+var decimals = new Array();
+decimals = ["1000","900","500","400","100","90","50","40","10","9","5","4","1"];
+
+var converter = function(numberInput) {
+
+  if (numberInput <= 0 || numberInput >= 4000) {
+    return numberInput;
+  }
+  };
 
 
-
-
-
-
-
+//old spec:
+// if (numberInput === "1") {
+//   return "I"
+// } else if (numberInput ==="2") {
+//   return "II"
+// } else if (numberInput ==="3") {
+//   return "III"
+// }
+// };
 
 
 //user interface
 
-$(document).ready(function(event) {
+$(document).ready(function() {
+  $("#roman-converter").submit(function(event) {
+    event.preventDefault();
 
+    var numberInput = parseInt($("#number-input").val());
 
+    // alert(numberInput)
+
+    var numberOutput = converter(numberInput);
+
+    $("#numeral-results").text(numberOutput);
+
+  });
 });
